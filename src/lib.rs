12b1986c3c454461c471e7c4718420d02ae55493
“CorentinVaillant” <c.vaillant848@gmail.com>
2024-04-09 22:00:12 +0200

    comit

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-#[cfg(test)]
 /*
 !TODO 
-    - print
-    - mutl
-    - ...
+- print
+- mutl
+- ...
 */
 
+#[cfg(test)]
 mod tests {
     use crate::matrix::Matrix;
 
@@ -20,23 +20,28 @@ mod tests {
 
         let expect_m: Matrix<f32,2,3> = Matrix::from(expect_tab);
 
+        println!("expect_m :{}",expect_m);
+
         assert_eq!(_m1 + _m2, expect_m);
         
     }
 }
 
 pub mod matrix {
+    use core::fmt;
 
-    use std::ops::*;
 
+    //definition of Matrix
     #[derive(PartialEq,Debug)]
     pub struct Matrix<T :Default, const N: usize, const M: usize>
     {
         inner: [[T; M]; N],
     }
 
+    //definition of a default 
     impl<T: std::default::Default + std::marker::Copy, const N: usize, const M: usize> Default
         for Matrix<T, N, M>
+    
     {
         fn default() -> Self {
             Self {
@@ -45,6 +50,7 @@ pub mod matrix {
         }
     }
 
+    //definition using a vec
     impl<T: std::default::Default + std::marker::Copy, const N: usize, const M: usize>
         From<Vec<Vec<T>>> for Matrix<T, N, M>
     {
@@ -67,6 +73,7 @@ pub mod matrix {
         }
     }
 
+    //definition using an array
     impl<T: std::default::Default, const N: usize, const M: usize> From<[[T; M]; N]>
         for Matrix<T, N, M>
     {
@@ -75,18 +82,51 @@ pub mod matrix {
         }
     }
 
-    impl<const N :usize, const M:usize> Add for Matrix<f32,N,M> {
-        type Output = Self;
-        fn add(self, rhs: Self) -> Self::Output {
-            let mut result = Self::default();
-            for (i, (row1, row2)) in self.inner.into_iter().zip(rhs.inner).enumerate(){
-                for (j,(val1, val2)) in row1.into_iter().zip(row2).enumerate(){
-                    result.inner[i][j] = val1 + val2;
+    // mark : BUG
+    /*implementation to use print
+    it can not display f for the moment, I try to understand why*/
+    impl<T :std::default::Default + fmt::Display, const N :usize, const M:usize> fmt::Display for Matrix<T,N,M>{
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            
+            match write!(f,""){
+                Ok(_) =>             
+                {for i in 0..self.inner.len(){
+                    println!();
+                    for j in 0..self.inner[i].len(){
+                        print!("{} ", self.inner[i][j]);
+                    }
+                }
+                println!();
+                Ok(())},
+                Err(e) => {
+                    print!("{}",e);
+                    Err(e)
                 }
             }
-            result
+
         }
         
-
+    }
+    
+    //definition for the operation with f32 as T
+    pub mod matrix_operation_f32{
+        use crate::matrix::Matrix;
+        use std::ops::*;
+        
+        //definition of the addition
+        impl<const N :usize, const M:usize> Add for Matrix<f32,N,M> {
+            type Output = Self;
+            fn add(self, rhs: Self) -> Self::Output {
+                let mut result = Self::default();
+                for (i, (row1, row2)) in self.inner.into_iter().zip(rhs.inner).enumerate(){
+                    for (j,(val1, val2)) in row1.into_iter().zip(row2).enumerate(){
+                        result.inner[i][j] = val1 + val2;
+                    }
+                }
+                result
+            }
+            
+    
+        }
     }
 }
